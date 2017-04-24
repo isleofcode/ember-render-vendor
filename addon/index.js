d4a11dcf90c580e32f45cbb4a09ac49c89023848
@anulman <anulman@gmail.com>
2017-04-24 14:18:06 -0400

    feat(addon): export addon api

---
+++ addon/index.js
@@ -0,0 +1,8 @@
+import Renderer from 'ember-render-vendor/renderer';
+import rendererFor from 'ember-render-vendor/utils/renderer-for';
+
+export default Renderer;
+export {
+  Renderer,
+  rendererFor
+};
