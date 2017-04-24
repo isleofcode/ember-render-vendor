0fe1eab8020b2a70088df2444653a55740f95089
@anulman <anulman@gmail.com>
2017-04-24 14:15:20 -0400

    feat(ember-electron): add e-electron initializer

---
+++ ember-electron/initializers/ember-render-vendor.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const RenderVendor = require('render-vendor').default;
+const WebSocket = require('ws');
+
+const getSubdirs = require('ember-render-vendor/lib/utils/get-subdirs');
+
+function cleanup() {
+  let socket = global['ember-render-vendor'] &&
+    global['ember-render-vendor'].socket;
+
+  if (socket instanceof WebSocket.Server) {
+    socket.close();
+  }
+
+  RenderVendor.shutdown();
+}
+
+module.exports = function initialize(app) {
+  let renderers = {};
+  let socket = new WebSocket.Server({
+    perMessageDeflate: false,
+    port: 8080
+  });
+
+  getSubdirs(path.join(process.cwd(), 'renderers'))
+    .forEach((dir) => {
+      let name = path.basename(dir);
+      let url = path.join(dir, 'index.html');
+
+      // todo: support passing other opts, e.g. zoomFactor / viewportSize?
+      renderers[name] = RenderVendor.create({ url });
+    });
+
+  global['ember-render-vendor'] = {
+    renderers,
+    socket
+  }
+
+  app.on('quit', cleanup);
+  process.on('uncaughtException', cleanup);
+}
